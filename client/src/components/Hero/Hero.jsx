ad34d1c0effb716f62c439c8d3e9a36c5e87a3fb
Manjinder <smanjinder96.ms@gmail.com>
2021-07-08 11:04:14 -0700

    added lottie

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import "./Hero.scss"
+import Lottie  from 'react-lottie';
+import hacker from "../../assets/hacker.json"
 
 function Hero() {
+    const hackerfile = {
+        loop: true,
+        autoplay: true,
+        animationData: hacker,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    }
     return (
-        <div className="hero">
-            <h3 className="hero__text">In the last year alone  __% of Americans have fallen victim to identity theft.</h3>
-            <button className="hero__button">SEARCH</button>
-        </div>
+        <section className="hero">
+            <div className="hero__left">
+                <h3 className="hero__text">In the last year alone  __% of Americans have fallen victim to identity theft.</h3>
+                <button className="hero__button">SEARCH</button>
+            </div>
+            <Lottie options={hackerfile} height={300} width={400} />
+        </section>
     )
 }
 
